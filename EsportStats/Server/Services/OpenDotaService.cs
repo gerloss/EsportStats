c5d55c7ea3609cc66a09f72c68153f68bb59ef36
gerloss <zhilo941@gmail.com>
2021-04-08 18:50:12 +0200

    Add partial implementation for hero stats service

---
+++ EsportStats/Server/Services/OpenDotaService.cs
@@ -0,0 +1,51 @@
+﻿using EsportStats.Server.Data;
+using EsportStats.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EsportStats.Server.Services
+{
+    public interface IOpenDotaService
+    {
+        public Task<Dictionary<Hero, int>> GetHeroStatsAsync(string userId);
+        public Task<Dictionary<Hero, int>> GetHeroStatsAsync(ulong steamId);
+    }
+
+    public class OpenDotaService : IOpenDotaService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public OpenDotaService(
+            IUnitOfWork unitOfWork,
+            IHttpClientFactory httpClientFactory)
+        {
+            _unitOfWork = unitOfWork;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        /// <summary>
+        /// Serves the hero statistics of the user with the given userid.
+        /// </summary>       
+        public async Task<Dictionary<Hero, int>> GetHeroStatsAsync(string userId)
+        {
+            var user = await _unitOfWork.Users.GetAsync(userId);
+            return await GetHeroStatsAsync(user.SteamId);
+        }
+
+        /// <summary>
+        /// Serves the hero statistics of the user with the given steamid64.
+        /// </summary>    
+        public async Task<Dictionary<Hero, int>> GetHeroStatsAsync(ulong steamId)
+        {
+            // TODO:
+            // Check database if we have up-to-date hero stats
+            // If not, call OpenDota for fresh stats: https://api.opendota.com/api/players/{steamid32}/heroes
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EsportStats/Shared/Common/CustomExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EsportStats.Shared.Common
+{
+    public static class UlongExtension
+    {
+        public static int ToSteam32(this ulong value)
+        {
+            // You can get a Steam32 Id by substracting this number from the 64 bit Id
+            ulong c = 76561197960265728;
+            return Convert.ToInt32(value - c);
+        }
+    }
+}

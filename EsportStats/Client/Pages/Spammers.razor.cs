2ee9d4cbf3630e56b4b243f8ddb415b3f47e2aca
gerloss <zhilo941@gmail.com>
2021-04-09 00:26:43 +0200

    Rewrite Spammers component

---
+++ EsportStats/Client/Pages/Spammers.razor.cs
@@ -0,0 +1,26 @@
+﻿using EsportStats.Shared.DTO;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace EsportStats.Client.Pages
+{
+    public partial class Spammers : ComponentBase
+    {
+        [Inject]
+        private HttpClient _http { get; set; }
+
+        IEnumerable<TopListEntryDTO> entries = new List<TopListEntryDTO>();
+        bool isLoaded = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            entries = await _http.GetFromJsonAsync<IEnumerable<TopListEntryDTO>>("/Api/Spammers");
+            isLoaded = true;
+        }
+    }
+}
